eb1c174536af2bbf38fabffd7488c4ed315bf120
Vaibhav Kabira <vaibhavkabira995@gmail.com>
2024-03-12 12:30:23 +0530

    update vite file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vaibhav-portfolio/",
+  base:"/portfolio_vaibhav_react/",
   plugins: [react()],
 })
